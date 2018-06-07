e13df8ef4169b252037be86f981b3a8c5f01059f
kkomazakii <komazaki@giftee.co>
2018-06-07 11:25:09 +0900

    initail commit

---
+++ src/heroku/core.clj
@@ -0,0 +1,22 @@
+(ns heroku.core
+  (:require [org.httpkit.server :as http])
+  (:gen-class))
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "application/json"}
+   :body    "{\"url\": \"https://filmarks.com/movies/71657\"}"})
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    ;; graceful shutdown: wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (reset! server (http/run-server #'app {:port 8080})))
---
+++ test/heroku/core_test.clj
@@ -0,0 +1,7 @@
+(ns heroku.core-test
+  (:require [clojure.test :refer :all]
+            [heroku.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
